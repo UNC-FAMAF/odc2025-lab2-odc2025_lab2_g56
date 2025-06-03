7405bc3847f6f37d5cb39c8e8b7d8d3f63ddc9b8
FacuFernandezGalvez <facundofernandez@mi.unc.edu.ar>
2025-06-03 17:53:18 -0300

    Finalizo la maquina pinball

--- ejercicio1/maquina_pinball.s
+++ ejercicio1/maquina_pinball.s
@@ -22,7 +22,7 @@
 // mov x2,x10
 maquina_pinball:
 //cuerpo
-    mov x25,lr
+    mov x22,lr
     mov x13,x1
     mov x14,x2
 
@@ -196,7 +196,6 @@ maquina_pinball:
     mov x5,#30
     bl pintar_rectangulo
 
-//pinto pantalla
     movz x3, 0x26, lsl 16
 	movk x3, 0x00AF, lsl 00
     mov x1,x13
@@ -206,7 +205,7 @@ maquina_pinball:
     mov x4,#50 
     mov x5,#1   
     bl pintar_rectangulo
-
+//pinto pantalla
 
 
     movz x3, 0x26, lsl 16
@@ -219,27 +218,129 @@ maquina_pinball:
     mov x5,#34  
     bl pintar_rectangulo
 
+   // movz x3, 0x6D, lsl 16
+	//movk x3, 0xEEF4, lsl 00
+    movz x3, 0x91, lsl 16
+	movk x3, 0x33ED, lsl 00
+    mov x1,x13
+    mov x2,x14
+    add x1,x1,#68
+    sub x2,x2,#86
+    mov x4,#36  
+    mov x5,#32  
+    bl pintar_rectangulo
+
     movz x3, 0x6D, lsl 16
 	movk x3, 0xEEF4, lsl 00
     mov x1,x13
     mov x2,x14
-    add x1,x1,#70
-    sub x2,x2,#84
-    mov x4,#33  
-    mov x5,#30  
+    add x1,x1,#72
+    sub x2,x2,#82
+    mov x4,#32  
+    mov x5,#28  
     bl pintar_rectangulo
 
+//pinto detallitos
+//primer "OJO" de la maquina
+    movz x3, 0x26, lsl 16
+	movk x3, 0x00AF, lsl 00
+    mov x1,x13
+    mov x2,x14
+    add x1,x1,#12
+    add x2,x2,#11
+    mov x6,#4
+    bl pintar_circulo
+
+    movz x3, 0xFF, lsl 16
+	movk x3, 0xDA47, lsl 00
+    mov x1,x13
+    mov x2,x14
+    add x1,x1,#3
+    add x2,x2,#3
+    mov x6,#2
+    bl pintar_circulo
+//otro "OJO"
+    movz x3, 0x26, lsl 16
+	movk x3, 0x00AF, lsl 00
+    mov x1,x13
+    mov x2,x14
+    add x1,x1,#36
+    add x2,x2,#2
+    mov x6,#4
+    bl pintar_circulo
+
+    movz x3, 0xFF, lsl 16
+	movk x3, 0xDA47, lsl 00
+    mov x1,x13
+    mov x2,x14
+    add x1,x1,#3
+    add x2,x2,#3
+    mov x6,#2
+    bl pintar_circulo
+
+//parte externa de la "BOCA"
+    movz x3, 0x26, lsl 16
+	movk x3, 0x00AF, lsl 00
+    mov x1,x13
+    mov x2,x14
+    sub x1,x1,#23
+    add x2,x2,#3
+    mov x6,#3
+    bl pintar_circulo
+
+    movz x3, 0x26, lsl 16
+	movk x3, 0x00AF, lsl 00
+    mov x1,x13
+    mov x2,x14
+    add x1,x1,#15
+    add x2,x2,#2
+    mov x6,#3
+    bl pintar_circulo
 
 
+    movz x3, 0x26, lsl 16
+	movk x3, 0x00AF, lsl 00
+    mov x1,x13
+    mov x2,x14
+    sub x1,x1,#10
+    sub x2,x2,#1
+    mov x4,#7  
+    mov x5,#12 
+    bl pintar_rectangulo_aux
+
+//parte interna de la "BOCA"
+    movz x3, 0xFF, lsl 16
+	movk x3, 0x54EB, lsl 00
+    mov x1,x13
+    mov x2,x14
+    sub x1,x1,#11
+    add x2,x2,#2
+    mov x6,#1
+    bl pintar_circulo
+    
+    movz x3, 0xFF, lsl 16
+	movk x3, 0x54EB, lsl 00
+    mov x1,x13
+    mov x2,x14
+    add x1,x1,#13
+    add x2,x2,#1
+    mov x6,#1
+    bl pintar_circulo
+
+    movz x3, 0xFF, lsl 16
+	movk x3, 0x54EB, lsl 00
+    mov x1,x13
+    mov x2,x14
+    sub x1,x1,#12
+    mov x4,#3
+    mov x5,#12
+    bl pintar_rectangulo_aux
 
 
+    
 
 
-
-
-
-
-    mov lr,x25
+    mov lr,x22
     br lr
 
 
