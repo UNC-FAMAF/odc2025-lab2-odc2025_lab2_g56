c7290680d1292992650fad5970f54e574cedc9fb
FacuFernandezGalvez <facundofernandez@mi.unc.edu.ar>
2025-06-04 13:08:52 -0300

    añado los numeros 0 2 y 5

--- ejercicio1/letras_y_numeros.s
+++ ejercicio1/letras_y_numeros.s
@@ -28,6 +28,7 @@
     add x2,x2,#2
     bl pintar_pixel_minimo
 */
+//_______________________________________________________________________________________________
 
 letra_o:
     mov x25,lr
@@ -81,10 +82,11 @@ letra_o:
     mov x4,#1
     mov x5,#9
     bl pintar_rectangulo
-
+//retorno
     mov lr,x25
     br lr 
-
+//_______________________________________________________________________________________________
+//_______________________________________________________________________________________________
 letra_d:
     mov x25,lr
     mov x13,x1
@@ -171,10 +173,11 @@ letra_d:
     mov x4,#9
     mov x5,#2
     bl pintar_rectangulo
-
+//retorno
     mov lr,x25
     br lr 
-
+//_______________________________________________________________________________________________
+//_______________________________________________________________________________________________
 letra_c:
     mov x25,lr
     mov x13,x1
@@ -210,89 +213,196 @@ letra_c:
     mov x4,#1
     mov x5,#11
     bl pintar_rectangulo
-
-    
-//
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//retorno
     mov lr,x25
     br lr 
-
-numero_2:
-    mov x25,lr
-
-    mov lr,x25
-    br lr 
+//_______________________________________________________________________________________________
+//_______________________________________________________________________________________________
 numero_0:
     mov x25,lr
+    mov x13,x1
+    mov x14,x2
+//primera mitad del 0, como si fuera una C
+    add x1,x1,#2
+    mov x4,#1
+    mov x5,#4
+    //linea horizontal de arriba
+    bl pintar_rectangulo
+    add x1,x1,#8
+    sub x2,x2,#2
+    bl pintar_pixel_minimo
+    //rectangulito 1x2 arriba
+    mov x1,x13
+    mov x2,x14
+    add x2,x2,#2
+    mov x4,#1
+    mov x5,#2
+    bl pintar_rectangulo
+    //linea vertical larga 
+    mov x4,#8
+    mov x5,#1
+    bl pintar_rectangulo
+    //rectangulito 1x2 abajo
+    mov x4,#1
+    mov x5,#2
+    bl pintar_rectangulo
+    //linea horizontal de abajo
+    add x1,x1,#2
+    mov x4,#1
+    mov x5,#5
+    bl pintar_rectangulo
+//termina primera mitad del 0
 
+//completo la parte derecha del 0
+    mov x1,x13
+    mov x2,x14
+    add x2,x2,#2
+    add x1,x1,#10
+    mov x4,#1
+    mov x5,#2
+    bl pintar_rectangulo
+    add x1,x1,#2
+    mov x4,#8
+    mov x5,#1
+    bl pintar_rectangulo
+    sub x1,x1,#2
+    mov x4,#1
+    mov x5,#2
+    bl pintar_rectangulo
+//retorno
     mov lr,x25
     br lr 
+//_______________________________________________________________________________________________
+//_______________________________________________________________________________________________
+numero_2:
+    mov x25,lr
+    mov x13,x1
+    mov x14,x2
+// linea horizontal 1
+    add x1,x1,#2
+    mov x4,#1
+    mov x5,#4
+    bl pintar_rectangulo
+    add x1,x1,#8
+    sub x2,x2,#2
+    bl pintar_pixel_minimo
+//rectangulitos para dar forma arriba
+    mov x1,x13
+    mov x2,x14
+    add x2,x2,#2
+    mov x4,#1
+    mov x5,#2
+    bl pintar_rectangulo
+    sub x2,x2,#2
+    add x1,x1,#10
+    bl pintar_rectangulo
+//puntito
+    mov x1,x13
+    mov x2,x14
+    add x2,x2,#4
+    bl pintar_pixel_minimo
+//palito vertical del 2
+    mov x1,x13
+    mov x2,x14
+    add x1,x1,#12
+    add x2,x2,#4
+    mov x4,#3
+    mov x5,#1
+    bl pintar_rectangulo
+//escalerita hacia abajo a la izquierda
+    sub x1,x1,#2
+    mov x4,#1
+    mov x5,#2
+    bl pintar_rectangulo
+    sub x1,x1,#2
+    mov x4,#1
+    mov x5,#2
+    bl pintar_rectangulo
+    sub x1,x1,#2
+    mov x4,#1
+    mov x5,#2
+    bl pintar_rectangulo
+    sub x1,x1,#2
+    mov x4,#1
+    mov x5,#2
+    bl pintar_rectangulo
+    sub x1,x1,#2
+    mov x4,#1
+    mov x5,#2
+    bl pintar_rectangulo
+    sub x1,x1,#2
+    mov x4,#1
+    mov x5,#2
+    bl pintar_rectangulo
+//base del 2
+    mov x4,#1
+    mov x5,#7
+    bl pintar_rectangulo
+//retorno
+    mov lr,x25
+    br lr 
+//_______________________________________________________________________________________________
+//_______________________________________________________________________________________________
 numero_5:
     mov x25,lr
+    mov x13,x1
+    mov x14,x2
 
+    mov x4,#1
+    mov x5,#7
+    bl pintar_rectangulo
+    mov x4,#4
+    mov x5,#1
+    bl pintar_rectangulo
+    mov x4,#1
+    mov x5,#6
+    bl pintar_rectangulo
+    add x1,x1,#10
+    mov x4,#1
+    mov x5,#2
+    bl pintar_rectangulo
+    add x1,x1,#2
+    mov x4,#3
+    mov x5,#1
+    bl pintar_rectangulo
+    sub x1,x1,#2
+    mov x4,#1
+    mov x5,#2
+    bl pintar_rectangulo
+    mov x1,x13
+    mov x2,x14
+    add x2,x2,#22
+    mov x4,#1
+    mov x5,#6
+    bl pintar_rectangulo
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+//retorno
     mov lr,x25
     br lr 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//mov color
-//pintas circulo
-
-//mov color
-//
